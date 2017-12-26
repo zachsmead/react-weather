0a33c8adc8e27635f2fdd826a70b3a9be0eacdf0
= <zrsmead@gmail.com>
2017-12-25 23:52:46 -0800

    merry christmas you filthy animal!

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,6 @@
+// this component is not a container because it doesn't care about application
+// state, SearchBar does.
+
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
