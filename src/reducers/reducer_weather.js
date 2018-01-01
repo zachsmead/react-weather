80eeb0014ab2eb15ae9c86847429b04bc59e161c
= <zrsmead@gmail.com>
2018-01-01 01:19:16 -0800

    notes on es6 syntax, handling return state in reducers to avoid state mutation

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,20 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
   console.log('Action received', action)
+  switch(action.type) {
+  case FETCH_WEATHER:
+    // return state.concat([action.payload.data]);
+    return [ action.payload.data, ...state ]; // this is a piece of ES6 syntax
+                                              // which says to return a new array,
+                                              // with the action payload as an item
+                                              // inside it, and then (the three
+                                              // dots say) this other object might
+                                              // be an array, so take its items
+                                              // and add them to the aforementioned
+                                              // array. so it flattens the inputs
+                                              // into a single array that gets
+                                              // returned.
+  }
   return state;
 }
