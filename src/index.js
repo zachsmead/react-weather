2d68c266a33c855fe62cbaade89dae5d127a1e91
= <zrsmead@gmail.com>
2017-12-31 20:47:37 -0800

    preliminary notes on middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise'; // this is a middleware package;
+                                          // middleware deals with actions
+                                          // made by action creators.
+                                          // middleware does something,
+                                          // really anything we want,
+                                          // to an action, before letting
+                                          // the action pass through
+                                          // to our reducers.
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// this line is where we actually hook up our middleware to the app.
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
