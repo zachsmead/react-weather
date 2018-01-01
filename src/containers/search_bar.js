73cf2e13f3a4b7dd2a07d38988f824b5b7e92ea4
= <zrsmead@gmail.com>
2017-12-31 19:52:15 -0800

    changed input form into controlled field and commented

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,10 +4,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' }
+	}
+
 	render() {
 		return (
 			<form className='input-group'>
-				<input />
+			<input
+				placeholder='Get a five-day forecast in your favorite cities'
+				className='formControl'
+				value={this.state.term}
+				onChange={this.onInputChange} />
+			// the lines above make our input form into a 'controlled field',
+			// which is form element where the value of the input is set
+			// by the state of our component, not the other way around.
+			// so when we click the button below it will draw its input
+			// from state.whatever, instead of directly from <input />.
 				<span className='input-group-btn'>
 					<button type='submit' className='btn btn-secondary'>Submit</button>
 				</span>
